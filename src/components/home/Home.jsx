83d5c53c30386bd1e8e6c1f3097ae69d6248037b
MINSU <ancx1234@naver.com>
2022-04-23 17:53:46 +0900

    Door 생성

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import "./Home.css";
 
 export const Home = () => {
   return (
-    <>
-      <h2>HOME</h2>
-    </>
+    <div className='background'>
+      <div className='container'>
+        <div className="toCar">
+          <img className='doorCar' src='./pic/Home/door.png'/>
+        </div>
+        <div className="toPhone">
+          <img className='doorCar' src='./pic/Home/door.png'/>
+        </div>
+        <div className="toGoods">
+          <img className='doorCar' src='./pic/Home/door.png'/> 
+        </div>
+      </div>
+    </div>
   )
 }
