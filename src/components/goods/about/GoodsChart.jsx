139ecff59bcd6bcc8842241347df4dea84a2719a
ByeongJu <39815497+youbj@users.noreply.github.com>
2022-05-27 22:58:22 +0900

    123

--- src/components/goods/about/GoodsChart.jsx
+++ src/components/goods/about/GoodsChart.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React,{useState} from "react";
 import "./GoodsChart.css";
 import { Chart } from "react-google-charts";
 import { Tab } from "react-bootstrap";
 import { Tabs } from "react-bootstrap";
+import{Nav} from "react-bootstrap";
 
 export const GoodsChart = () => {
   const data = [
@@ -16,18 +17,23 @@ export const GoodsChart = () => {
     is3D: false,
   };
 
-  const Clothedata = [
+  const Clothedata1 = [
     ["Age Group", "18-27", "28-37", "38-47"],
     ["Audi Men's Hooded Windbreaker", 87, 103, 48],
     ["Descent Swiss (Audi) Ski Original Short Down Jacket", 108, 82, 25],
     ["Bentley Jersey",  19, 34, 68],
     ["Benz PK Polo Golf Collar Shirt", 31, 41, 103],
+  ];
+
+  const Clothedata2 =[
+    ["Age Group", "18-27", "28-37", "38-47"],
     ["Benz T-Shirt Short-Sleeved", 51, 67, 15],
     ["BMW Men's Hood Tech Short Jacket", 73, 51, 50],
     ["Men’s recycled scuba sweatshirt with Ferrari logo", 51, 67, 15],
     ["Organic cotton T-shirt with Ferrari logo", 82, 93, 68],
     ["Volvo Jersey", 17, 35, 47],
   ];
+
   const Clotheoptions = {
     chart: {
       title: "Clothes Chart",
@@ -35,18 +41,21 @@ export const GoodsChart = () => {
     },
   };
 
-  const Accessoriesdata = [
+  const Accessoriesdata1 = [
     ["Age Group", "18-27", "28-37", "38-47"],
-    ["Benz Men's Perfume MB Man", 83, 103, 44],
-    ["Light Essence By Ferrari For Men", 54, 105, 115],
-    ["Tonino Lamborghini Invincible EDT", 65, 65, 100],
-    ["Bentley Azur EDT", 85, 39, 99],
-    ["Mercedes-Benz Ring", 105, 118, 88],
-    ["PhoneNum KeyRing", 105, 118, 59],
-    ["Benz Las Vegas Key Ring", 73, 85, 59],
-    ["Leather Prancing Horse keychain", 1, 85, 32],
-    ["Bride Racing Wallet", 17, 47, 2],
-    ["Legend Car Logo Wallet", 120, 72, 35],
+    ["Benz Men's \nPerfume MB Man", 83, 103, 44],
+    ["Light Essence By \nFerrari For Men", 54, 105, 115],
+    ["Tonino Lamborghini \nInvincible EDT", 65, 65, 100],
+    ["Bentley \nAzur EDT", 85, 39, 99],
+    ["Mercedes-Benz \nRing", 105, 118, 88],
+  ];
+  const Accessoriesdata2 =[
+    ["Age Group", "18-27", "28-37", "38-47"],
+    ["PhoneNum \nKeyRing", 105, 118, 59],
+    ["Benz Las Vegas\n Key Ring", 73, 85, 59],
+    ["Leather Prancing \nHorse keychain", 1, 85, 32],
+    ["Bride\n Racing Wallet", 17, 47, 2],
+    ["Legend Car \nWallet", 120, 72, 35],
   ];
 
   const Accessoriesoptions = {
@@ -56,19 +65,23 @@ export const GoodsChart = () => {
     },
   };
 
-  const Phonecasedata = [
+  const Phonecasedata1 = [
     ["Age Group", "18-27", "28-37", "38-47"],
-    ["Car Printing Case", 47,97, 5],
-    ["F1 Racing Case", 53, 63, 73],
-    ["Lamborghini Logo Case", 15, 28, 90],
-    ["Korea Independence Case", 110, 158, 170],
-    ["Thisisneverthat Case", 102, 130, 11],
-    ["Volkswagen Logo Case", 44, 17, 14],
-    ["CASETiFY Flip Lettering Case", 106, 115, 89],
-    ["CASETiFY S21 Cat Case", 100, 102, 93],
-    ["Iphone Case Mac Safe", 109, 111, 14],
-    ["CASETiFY Iphone Case", 80, 106, 16],
-    ["Iphone Case portrait", 71, 107, 33],
+    ["Car \nCase", 47,97, 5],
+    ["F1 \nRacing Case", 53, 63, 73],
+    ["Lamborghini \nCase", 15, 28, 90],
+    ["Korea \nIndependence Case", 110, 158, 170],
+    ["Thisisneverthat \nCase", 102, 130, 11],
+
+  ];
+  const Phonecasedata2 =[
+    ["Age Group", "18-27", "28-37", "38-47"],
+    ["Volkswagen \nLogo Case", 44, 17, 14],
+    ["CASETiFY  \nLettering Case", 106, 115, 89],
+    ["CASETiFY  \nCat Case", 100, 102, 93],
+    ["Iphone Case \nMac Safe", 109, 111, 14],
+    ["CASETiFY \n Case", 80, 106, 16],
+    ["Iphone Case \nportrait", 71, 107, 33],
   ];
   const Phonecaseoptions = {
     chart: {
@@ -77,8 +90,9 @@ export const GoodsChart = () => {
     },
   };
 
+
   return (
-    <Tabs defaultActiveKey={1} id="uncontrolled ">
+    <Tabs defaultActiveKey={2} id="uncontrolled ">
       <Tab eventKey={1} title="gender">
         <Chart
           chartType="PieChart"
@@ -88,33 +102,66 @@ export const GoodsChart = () => {
           options={options}
         />
       </Tab>
+
       <Tab eventKey={2} title="Accessories">
         <Chart
           chartType="Bar"
           width="1300px"
           height="500px"
-          data={Accessoriesdata}
+          data={Accessoriesdata1}
           options={Accessoriesoptions}
         />
-      </Tab>
-      <Tab eventKey={3} title="Phone Case">
         <Chart
           chartType="Bar"
           width="1300px"
           height="500px"
-          data={Phonecasedata}
-          options={Phonecaseoptions}
+          data={Accessoriesdata2}    
         />
       </Tab>
-      <Tab eventKey={4} title="Clothes">
-      <Chart
+
+      <Tab eventKey={3} title="Phone Case" >
+        <Chart
           chartType="Bar"
           width="1300px"
           height="500px"
-          data={Clothedata}
+          data={Phonecasedata1}
+          options={Phonecaseoptions}
+        />
+        <Chart
+          chartType="Bar"
+          width="1300px"
+          height="500px"
+          data={Phonecasedata2}
+        />
+      </Tab>
+
+      <Tab eventKey={4} title="Clothes">
+        <Chart
+          chartType="Bar"
+          width="1300px"
+          height="500px"
+          data={Clothedata1}
           options={Clotheoptions}
+        />
+        <Chart
+          chartType="Bar"
+          width="1300px"
+          height="500px"
+          data={Clothedata2}
         />
       </Tab>
     </Tabs>
+  //   <Nav className="mt-5 mb-3" variant="tabs" defaultActiveKey="link-0">
+  //   <Nav.Item>
+  //     <Nav.Link eventKey="link-0" onClick={()=>{TabContent(0)}}>Tab 1</Nav.Link>
+  //   </Nav.Item>
+  //   <Nav.Item>
+  //     <Nav.Link eventKey="link-1" onClick={()=>{TabContent(1)}}>Tab 2</Nav.Link>
+  //   </Nav.Item>
+  //   <Nav.Item>
+  //     <Nav.Link eventKey="link-2" onClick={()=>{TabContent(2)}}>Tab 3</Nav.Link>
+  //   </Nav.Item>
+  // </Nav>
+
   );
 };
