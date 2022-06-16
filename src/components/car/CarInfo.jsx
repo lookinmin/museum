eb199f8c5141917fcf656f2f99139a636babddf5
kkm0406 <sdjmc30412@naver.com>
2022-06-16 15:23:45 +0900

    0616

--- src/components/car/CarInfo.jsx
+++ src/components/car/CarInfo.jsx
@@ -11,6 +11,7 @@ import {
   useScroll,
   OrbitControls,
   Text,
+  Text3D,
   Stars,
 } from "@react-three/drei";
 import { useRoute, useLocation } from "wouter";
@@ -29,32 +30,29 @@ const extrudeSettings = {
   bevelThickness: 0.25,
 };
 
-
-
-const Car=({carpos})=>{
-  const scroll = useScroll()
-  const temp=useRef(new THREE.Vector3(0,-0.5,4))
-  const tmp=useRef()
+const Car = ({ carpos }) => {
+  const scroll = useScroll();
+  const temp = useRef(new THREE.Vector3(0, -0.5, 4));
+  const tmp = useRef();
   useFrame((state, delta) => {
-    tmp.current.position.z=scroll.scroll.current*-10+5
-    tmp.current.position.y=-0.5
-    tmp.current.position.x=0
-    carpos.current=tmp.current.position
-  })
-  return(
-    <group position={[0,-0.5,4]} ref={tmp}>
+    tmp.current.position.z = scroll.scroll.current * -10 + 5;
+    tmp.current.position.y = -0.5;
+    tmp.current.position.x = 0;
+    carpos.current = tmp.current.position;
+  });
+  return (
+    <group position={[0, -0.5, 4]} ref={tmp}>
       <Car_model pos={temp}></Car_model>
     </group>
-          
-  )
-}
+  );
+};
 
 export const CarInfo = ({ images }) => {
   const [set, reSet] = useState(false);
   const modal = useRef();
   const [modalFlag, setModalFlag] = useState(false);
-  const car_position=useRef(new THREE.Vector3(0.2, 0.205, 4.1));
-  const flag=useRef(true);
+  const car_position = useRef(new THREE.Vector3(0.2, 0.205, 4.1));
+  const flag = useRef(true);
 
   const carInfo = useRef();
   const carImg = useRef();
@@ -90,12 +88,15 @@ export const CarInfo = ({ images }) => {
   }, [modalFlag]);
   return (
     <>
-      <Canvas gl={{ alpha: false }} camera={{ fov: 70, position: [0.2, 0.205, 4.1] }}>
-      <ScrollControls pages={10}>
-        <Car carpos={car_position}></Car>  
+      <Canvas
+        gl={{ alpha: false }}
+        camera={{ fov: 70, position: [0.2, 0.205, 4.1] }}
+      >
+        <ScrollControls pages={10}>
+          <Car carpos={car_position}></Car>
         </ScrollControls>
-        
-       <rectAreaLight
+
+        <rectAreaLight
           width={10}
           height={10}
           color={"#0000ff"}
@@ -115,12 +116,17 @@ export const CarInfo = ({ images }) => {
           rotation={[0, Math.PI * 0.5, 0]}
           castShadow
         />
-        
+
         <color attach="background" args={["#191920"]} />
         <fog attach="fog" args={["#191920", 0, 15]} />
         <Environment preset="city" />
         <group position={[0, -0.5, 0]}>
-          <Car_Museum flag={flag} car_position={car_position} images={images} modalClick={modalClick} />
+          <Car_Museum
+            flag={flag}
+            car_position={car_position}
+            images={images}
+            modalClick={modalClick}
+          />
           <mesh rotation={[-Math.PI * 0.5, 0, 0]}>
             <planeGeometry args={[50, 50]} />
             <MeshReflectorMaterial
@@ -166,10 +172,10 @@ export const CarInfo = ({ images }) => {
 //   // useFrame((state, dt) => {//화면 프레임에 따라 다름 144hz면 초당 144 60hz면 초당 60번 불림
 //   //   // state.camera.position.lerp(pos.current, 0.015)//부드러운 화면 카메라 위치 전환
 //   //   p1.lerp(pos1.current,0.015);//부드러운 카메라 표적 위치 변환
-    
+
 //   // })
 //   return (
-    
+
 //   )
 // }
 const ToHall = (path, setLocation) => {
@@ -187,65 +193,78 @@ const ClickFrame = (current, object, setLocation) => {
   setLocation(current === object ? "/car" : "/car/" + object.name);
 };
 
-function Car_Museum({images, q = new THREE.Vector3(0.18, 0.205, 3.61), modalClick, car_position,flag}) {
+function Car_Museum({
+  images,
+  q = new THREE.Vector3(0.18, 0.205, 3.61),
+  modalClick,
+  car_position,
+  flag,
+}) {
   const ref = useRef();
   const [, params] = useRoute("/car/:id");
   const [, setLocation] = useLocation();
   var click;
-  var camera_pos=car_position.current
-  var temp_pos
-  var frame_click=new THREE.Vector3();
-  const camerapoint=useRef()
+  var camera_pos = car_position.current;
+  var temp_pos;
+  var frame_click = new THREE.Vector3();
+  const camerapoint = useRef();
   useEffect(() => {
     click = ref.current.getObjectByName(params?.id);
     if (click) {
-      flag.current=false;
+      flag.current = false;
       let num = click.parent.num;
       console.log(click.parent.position);
       console.log(num);
       if (num % 2 === 0) {
-        frame_click.y=click.parent.position.y+0.2
-        frame_click.x=click.parent.position.x-1.5
-        frame_click.z=click.parent.position.z
+        frame_click.y = click.parent.position.y + 0.2;
+        frame_click.x = click.parent.position.x - 1.5;
+        frame_click.z = click.parent.position.z;
         click.parent.updateWorldMatrix(true, true);
-        click.parent.localToWorld(temp_pos.set(0,0.8,0));
+        // click.parent.localToWorld(temp_pos.set(0, 0.8, 0));
+        var p = new THREE.Vector3();
+        click.parent.localToWorld(p.set(1, 1.6/2, 1.25));
       } else {
-        frame_click.y=click.parent.position.y+0.2
-        frame_click.x=click.parent.position.x+1.5
-        frame_click.z=click.parent.position.z
+        frame_click.y = click.parent.position.y + 0.2;
+        frame_click.x = click.parent.position.x + 1.5;
+        frame_click.z = click.parent.position.z;
         click.parent.updateWorldMatrix(true, true);
-        click.parent.localToWorld(temp_pos.set(0,0.8,0));
+        click.parent.localToWorld(temp_pos.set(0, 0.8, 0));
       }
       modalClick(num);
     } else {
-      flag.current=true;
+      flag.current = true;
       camera_pos.set(0.2, 0.205, 4.1); //초기 시점 좌표
       modalClick(-2);
     }
   });
-  
-  useFrame((state) => {
-    if(flag.current){
-      camera_pos=car_position.current
-    // pos1.current.setX(0+state.mouse.x*4)
-    // pos1.current.setY(0-state.mouse.y*4)
-    state.camera.position.set(camera_pos.x+0.2,camera_pos.y+0.705,camera_pos.z)
-    temp_pos=new THREE.Vector3(camera_pos.x+0.18,camera_pos.y+0.705,camera_pos.z-0.49)
-    temp_pos.x=temp_pos.x+state.mouse.x*0.75
-    temp_pos.y=temp_pos.y+state.mouse.y*0.1
-    camerapoint.current.position.set(temp_pos.x,temp_pos.y,temp_pos.z)
-    state.camera.lookAt(temp_pos);
-    }
-    else{
-    // state.camera.position.lerp(frame_click, 0.025);
-    state.camera.lookAt(temp_pos);
-    }
 
+  useFrame((state) => {
+    if (flag.current) {
+      camera_pos = car_position.current;
+      // pos1.current.setX(0+state.mouse.x*4)
+      // pos1.current.setY(0-state.mouse.y*4)
+      state.camera.position.set(
+        camera_pos.x + 0.2,
+        camera_pos.y + 0.705,
+        camera_pos.z
+      );
+      temp_pos = new THREE.Vector3(
+        camera_pos.x + 0.18,
+        camera_pos.y + 0.705,
+        camera_pos.z - 0.49
+      );
+      temp_pos.x = temp_pos.x + state.mouse.x * 0.75;
+      temp_pos.y = temp_pos.y + state.mouse.y * 0.1;
+      camerapoint.current.position.set(temp_pos.x, temp_pos.y, temp_pos.z);
+      state.camera.lookAt(temp_pos);
+    } else {
+      // state.camera.position.lerp(frame_click, 0.025);
+      state.camera.lookAt(temp_pos);
+    }
   });
   return (
     <>
       <group
-      
         ref={ref}
         onClick={
           (e) =>
@@ -255,9 +274,9 @@ function Car_Museum({images, q = new THREE.Vector3(0.18, 0.205, 3.61), modalClic
         }
         onPointerMissed={() => setLocation("/car")}
       >
-        <Box args={[0.5,0.5,0.5]} ref={camerapoint} position={temp_pos}>
-      <meshBasicMaterial  color={"#ff0000"}></meshBasicMaterial>
-    </Box>
+        <Box args={[0.5, 0.5, 0.5]} ref={camerapoint} position={temp_pos}>
+          <meshBasicMaterial color={"#ff0000"}></meshBasicMaterial>
+        </Box>
         {images.map((props) => (
           <Photo_Frame key={props.url} {...props} />
         ))}
@@ -318,6 +337,7 @@ function Photo_Frame({ url, ...props }) {
         position={[0.55, 1.6, 0]}
         fontSize={0.08}
         color="#c0c0c0"
+        font="https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2201-2@1.0/LeferiPoint-BlackObliqueA.woff"
       >
         {name}
       </Text>
